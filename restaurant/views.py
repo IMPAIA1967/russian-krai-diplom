c9040981d410a1ca7e96c875eb98fcb9e129d9d7
Dmitry <dima.soin2014@yandex.ru>
2026-02-21 17:54:28 +0300

    Добавлена команда в контекст главной страницы

--- restaurant/views.py
+++ restaurant/views.py
@@ -124,6 +124,9 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['page_title'] = 'Главная'
+        # Добавляем команду ресторана
+        from .models import TeamMember
+        context['team_members'] = TeamMember.objects.filter(is_active=True).order_by('order', 'last_name')[:8]
         return context
 
 
